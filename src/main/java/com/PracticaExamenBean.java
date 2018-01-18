92c7d868a5542a82f173de3069f3b777c49973f7
Dani <Dani@DESKTOP-N5R5R04>
2018-01-18 19:24:04 +0100

    lololo

---
+++ src/main/java/com/PracticaExamenBean.java
@@ -0,0 +1,15 @@
+package com;
+ 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+import java.io.Serializable;
+ 
+@ManagedBean
+@SessionScoped
+public class PracticaExamenBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+ 
+}
